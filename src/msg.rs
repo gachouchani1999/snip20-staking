4872233717eda48b8636888f8b2df95583a09eb2
Georges Chouchani <gachouchani@ndu.edu.lb>
2022-02-11 17:40:54 +0200

    initial SCRT smart contract

---
+++ src/msg.rs
@@ -0,0 +1,33 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use cosmwasm_std::{Uint128, Binary};
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InitMsg {
+    pub count: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleMsg {
+    // Receive SNIP20 tokens
+    Receive {
+        sender: String,
+        from: String,
+        amount: Uint128,
+        #[serde(default)]
+        msg: Option<Binary>,
+    },  
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CountResponse {
+    pub count: i32,
+}
