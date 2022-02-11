9052f040f0df611f5c13c6bd9dd9d5c75b05b1f6
Georges Chouchani <gachouchani@ndu.edu.lb>
2022-02-11 17:52:17 +0200

    receive snip20 tokens

--- src/msg.rs
+++ src/msg.rs
@@ -1,9 +1,10 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use cosmwasm_std::{Uint128, Binary};
+use cosmwasm_std::{Uint128, Binary, HumanAddr, Coin};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub count: i32,
+    pub snip20_hash: String,
+    pub snip20_address: String
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -26,8 +27,16 @@ pub enum QueryMsg {
     GetCount {},
 }
 
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+
+/// info needed to perform a callback message after instantiation
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub struct PostInitCallback {
+    /// the callback message to execute
+    pub msg: Binary,
+    /// address of the contract to execute
+    pub contract_address: HumanAddr,
+    /// code hash of the contract to execute
+    pub code_hash: String,
+    /// list of native Coin to send with the callback message
+    pub send: Vec<Coin>,
 }
--- src/state.rs
+++ src/state.rs
@@ -8,7 +8,6 @@ pub static CONFIG_KEY: &[u8] = b"config";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
     pub owner: CanonicalAddr,
 }
 
